ba2ae9140d3a0e5a17307f1b48cffc875682fa1a
Ross Massey <massey0ross@gmail.com>
2024-04-08 19:35:23 -0700

    add session settings

--- api/settings.py
+++ api/settings.py
@@ -198,3 +198,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# session settings
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
